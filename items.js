052ad5ce68d1c2aeded518d969db4782099bae3a
moustafa <moustafaroushdy1@gmail.com>
2022-04-06 12:12:04 +0200

    items in seperate file

---
+++ items.js
@@ -0,0 +1,5 @@
+[
+             {title:'water',price:10, image: 'water.jpg',total:0},
+			 {title: 'Coffee', price:70 , image: 'coffee.jpg',total:0},
+			 {title: 'Mango juice', price: 30, image:"mango-juice.jpg",total:0},
+]
